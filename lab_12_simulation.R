8b7b387ca51db8ecd6d7c4c2bebbfbdbc56877e1
MichaelLinCMU <mllin@andrew.cmu.edu>
2018-04-18 11:34:07 -0400

    run_simulation

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -6,7 +6,6 @@ generate_data = function(n, p){
   }
   return(list(covariates=covariates, responses=responses))
 }
-generate_data(5,5)
 
 model_select = function(covariates, responses, cutoff){
   responses = responses[which(covariates <cutoff)]
@@ -14,5 +13,12 @@ model_select = function(covariates, responses, cutoff){
   if(length(covariates)==0) return(vector("numeric", length = 0))
   return(summary(lm(responses ~ covariates))[4])
 }
+model_select(generate_data(5,5)$covariates, generate_data(5,5)$responses, .5)
 
-model_select(generate_data(5,5)$covariates,generate_data(5,5)$responses, .5 )
+run_simulation = function(n_trials, n, p, cutoff){
+  for (i in 1:n_trials){
+    hist(model_select(generate_data(n,p)$covariates, generate_data(n,p)$responses, cutoff, breaks = 5))
+  }
+}
+
+run_simulation(3,3,5,.5)
