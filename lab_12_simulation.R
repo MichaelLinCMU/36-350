e90f864f169307b4fc0e549370d3a6d7868d8a39
MichaelLinCMU <mllin@andrew.cmu.edu>
2018-04-18 09:50:50 -0400

    model_select fn

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,6 +4,15 @@ generate_data = function(n, p){
   for (i in 1:n){
     responses[i] = rnorm(1,0,1)
   }
-  return(list(covariates, responses))
+  return(list(covariates=covariates, responses=responses))
 }
 generate_data(5,5)
+
+model_select = function(covariates, responses, cutoff){
+  responses = responses[which(covariates <cutoff)]
+  covariates = covariates[which(covariates <cutoff)]
+  if(length(covariates)==0) return(vector("numeric", length = 0))
+  return(summary(lm(responses ~ covariates))[4])
+}
+
+model_select(generate_data(5,5)$covariates,generate_data(5,5)$responses, .5 )
